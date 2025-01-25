d953a8f1486ce7d41e754c13712c6f8dc0e9f0f7
zfr_nytrox <155957683+nimgati@users.noreply.github.com>
2025-01-25 14:35:03 +0100

    Update main.py

--- NumeralRush_app/main.py
+++ NumeralRush_app/main.py
@@ -1,7 +1,7 @@
 import sys
 import NumeralRush_app.app
 import NumeralRush_app.opt
-
+# mise a jour 1
 if __name__ == '__main__':
     # definir les argument de application
     NumeralRush_app.opt.args = sys.argv
