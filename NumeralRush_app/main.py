2f0ef4d4804165752ec73edb38e3ea357e102323
nimgati <17164.fichier47@gmail.com>
2025-01-25 16:21:47 +0100

    initial commit

--- NumeralRush_app/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import NumeralRush_app.app
-import NumeralRush_app.opt
-
-if __name__ == '__main__':
-    # definir les argument de application
-    NumeralRush_app.opt.args = sys.argv
-    application = NumeralRush_app.app.Application()
-    application.run()
